0fdae085a6014aed1c2582f39d41f90075fc1d38
hspintog <59073704+hspintog@users.noreply.github.com>
2021-11-21 17:51:18 -0500

    create app flutter and showLogo, showEmail, showButtons

---
+++ vehicles_app/lib/screens/login_screen.dart
@@ -0,0 +1,137 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({ Key? key }) : super(key: key);
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  String _email = '';
+  String _password = ''; 
+  bool _rememberme = true;  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            _showLogo(),
+            SizedBox(height: 20,),
+            _showEmail(),
+            _showPassword(),
+            _showRememberme(),
+            _showButtons(),
+          ],
+        )
+      ),
+    );
+  }
+
+  Widget _showLogo() {
+    return Image(
+      image: AssetImage('assets/vehicles_logo.png'),
+      width: 300,
+    );
+  }
+
+  Widget _showEmail() {
+      return Container(
+      padding: EdgeInsets.all(10),
+        child: TextField(
+          autofocus: true,
+          keyboardType: TextInputType.emailAddress,
+          decoration: InputDecoration(
+            hintText: 'Ingresa tu email...',
+            labelText: 'Email',
+            suffixIcon: Icon(Icons.email),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10)
+            )
+          ),
+          onChanged: (value){
+            _email = value;
+          },
+        ),
+      );
+  }
+
+  Widget _showPassword() {
+    return Container(
+      padding: EdgeInsets.all(10),
+        child: TextField(
+          obscureText: true,
+          decoration: InputDecoration(
+            hintText: 'Ingresa tu contraseña...',
+            labelText: 'Contraseña',
+            suffixIcon: Icon(Icons.lock),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10)
+            )
+          ),
+          onChanged: (value){
+            _password = value;
+          },
+        ),
+      );
+  
+  }
+
+  Widget _showRememberme() {
+    return CheckboxListTile(
+      title: Text('Recordarme'),
+      value: _rememberme,
+      onChanged: (value){
+        setState(() {
+          _rememberme = value!;
+        });
+      },
+    );
+
+  }
+
+  Widget _showButtons() {
+    return Container(
+      margin: EdgeInsets.only(left: 10, right: 10),
+      child: Row(
+        
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: <Widget>[
+          Expanded(
+            child: ElevatedButton(
+              child: Text('Iniciar Session'),
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.resolveWith<Color>(
+                  (Set<MaterialState> states){
+                    return Colors.orange;
+                  }
+                  ),
+              ),  
+              onPressed: (){}, 
+              
+            ),
+          ),
+          SizedBox(width: 20,),
+          Expanded(
+            child: ElevatedButton(
+              child: Text('Nuevo Usuario'),
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.resolveWith<Color>(
+                  (Set<MaterialState> states)
+                  {
+                    return Colors.purple;
+                  }
+                ),
+              ),
+              onPressed: (){}, 
+              
+            ),
+          ),
+        ],
+      ),
+    );
+
+  }
+}
